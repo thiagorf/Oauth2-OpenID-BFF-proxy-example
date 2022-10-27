3817eafd54b9eee99fcb73babe3fea55002456ab
thiagorf <trfarciroli@gmail.com>
2022-10-27 15:41:17 -0300

    authZ code request logic

--- packages/client/src/pages/Me.tsx
+++ packages/client/src/pages/Me.tsx
@@ -1,20 +1,7 @@
-import { Provider } from "../components/provider";
-import { providers } from "../core/providers";
-
 export const Me = () => {
-    console.log(providers);
-
     return (
         <div>
             <h1>Me Page</h1>
-
-            {providers.map((provider, index) => (
-                <Provider
-                    key={index}
-                    provider={provider.provider}
-                    url={provider.url}
-                />
-            ))}
         </div>
     );
 };
